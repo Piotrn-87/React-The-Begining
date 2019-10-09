6645f7daac3bbef37108c8029f6220c36c511ec3
Piotr Napieralski <piotrn-87@o2.pl>
2019-10-09 21:56:30 +0200

    Feat: Extract EditableTimebox component

--- timeboxing/src/components/remaining-components.js
+++ timeboxing/src/components/remaining-components.js
@@ -1,59 +1,7 @@
 import React from "react";
-import TimeboxEditor from "./TimeboxEditor";
-import CurrentTimebox from "./CurrentTimebox";
+import EditableTimebox from "./EditableTimebox";
 
 import uuid from "uuid";
-
-class EditableTimebox extends React.Component {
-  state = {
-    isEditable: true,
-    title: "Ucze sie Reacta",
-    totalTimeInMinutes: 20
-  };
-
-  handleTitleChange = event => {
-    this.setState({
-      title: event.target.value
-    });
-  };
-  handleTotalTimeInMinutesChange = event => {
-    this.setState({
-      totalTimeInMinutes: event.target.value
-    });
-  };
-  handleConfirm = event => {
-    this.setState({
-      isEditable: false
-    });
-  };
-  handleEdit = evetn => {
-    this.setState({
-      isEditable: true
-    });
-  };
-
-  render() {
-    const { isEditable, title, totalTimeInMinutes } = this.state;
-    return (
-      <React.Fragment>
-        <TimeboxEditor
-          title={title}
-          totalTimeInMinutes={totalTimeInMinutes}
-          isEditable={isEditable}
-          onConfirm={this.handleConfirm}
-          onTitleChange={this.handleTitleChange}
-          onTotalTimeInMinutesChange={this.handleTotalTimeInMinutesChange}
-        />{" "}
-        <CurrentTimebox
-          isEditable={isEditable}
-          title={title}
-          totalTimeInMinutes={totalTimeInMinutes}
-          onEdit={this.handleEdit}
-        />{" "}
-      </React.Fragment>
-    );
-  }
-}
 
 class TimeboxCreator extends React.Component {
   constructor(props) {
