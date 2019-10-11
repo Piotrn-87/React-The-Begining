78df046990960e49e8fedeb95bf3033ea4683f69
Piotr Napieralski <piotrn-87@o2.pl>
2019-10-11 17:25:34 +0200

    Feat: create Header compontent

--- timeboxing/src/components/App.js
+++ timeboxing/src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TimeboxList from "./TimeboxList";
 import EditableTimebox from "./EditableTimebox";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <TimeboxList />
       <EditableTimebox />
     </div>
---
+++ timeboxing/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+  return (
+    <h1 className="header-text">Time to learn React. Once a day, every day</h1>
+  );
+}
+
+export default Header;
